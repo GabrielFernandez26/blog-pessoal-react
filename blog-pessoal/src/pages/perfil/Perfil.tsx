8515fef7a8b93d0678cc5fe6877ff6922db4a996
gabriel <gabriel.f.espanhol@hotmail.com>
2024-02-28 13:44:10 -0300

    Mudanças de estilização nas imagens

--- blog-pessoal/src/pages/perfil/Perfil.tsx
+++ blog-pessoal/src/pages/perfil/Perfil.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
-import loginLogo from '../../assets/login.jpg'
+import loginLogo from '../../assets/blog-login.jpg'
 import { toastAlert } from '../../utils/ToastAlert'
 function Perfil() {
   let navigate = useNavigate()
